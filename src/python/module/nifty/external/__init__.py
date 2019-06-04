c2cf2cf450a4477d73ff3e3c38356d3d6edfa7fc
Alberto Bailoni <bailoni.alberto@gmail.com>
2019-06-04 16:15:46 +0200

    Clean-up accumulate: move simplex noise and affAccumulation

--- src/python/module/nifty/external/__init__.py
+++ src/python/module/nifty/external/__init__.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from . import _external as __external
 from ._external import *
 
+import numpy
+
 __all__ = []
 for key in __external.__dict__.keys():
     try:
@@ -9,3 +11,35 @@ for key in __external.__dict__.keys():
     except:
         pass
     __all__.append(key)
+
+
+def generate_opensimplex_noise(shape, seed=None, features_size=1., number_of_threads=-1):
+    """
+    :param features_size: Increase this value to get bigger spatially consistent features. Decrease it to get results
+            similar to random Gaussian noise. Int or tuple
+    """
+    if not isinstance(shape, list):
+        shape = list(shape)
+    ndims = len(shape)
+
+    if seed is None:
+        seed = int(numpy.random.randint(-1000000000, 1000000000))
+
+    if isinstance(features_size, float):
+        features_size = numpy.ones(ndims, dtype="float32") * features_size
+    else:
+        assert isinstance(features_size, numpy.ndarray)
+        assert features_size.shape[0] == ndims
+
+    features_size = features_size.astype('float64')
+    assert all(features_size > 0.)
+
+
+    output = evaluateSimplexNoiseOnArray_impl(
+        shape=shape,
+        seed=seed,
+        featureSize=features_size,
+        numberOfThreads=number_of_threads
+    )
+
+    return output
