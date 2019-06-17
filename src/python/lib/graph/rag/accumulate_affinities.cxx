09c16e0c2fc09153dae3cc8077c1eb4e04a756fd
Alberto Bailoni <bailoni.alberto@gmail.com>
2019-06-17 15:48:18 +0200

    Temporary add back accumulateAffinitiesMeanAndLengthOnNodes()

--- src/python/lib/graph/rag/accumulate_affinities.cxx
+++ src/python/lib/graph/rag/accumulate_affinities.cxx
@@ -206,6 +206,100 @@ namespace graph{
                       py::arg("numberOfThreads") = -1
 
         );
+        ragModule.def("accumulateAffinitiesMeanAndLengthOnNodes",
+                      [](
+                              const GRAPH &graph,
+                              // TODO: generalize for more label types:
+                              xt::pytensor<int64_t, DIM> labels, // Labels less then zero are ignored
+                              xt::pytensor<DATA_T, DIM + 1> affinities,
+                              xt::pytensor<int, 2> offsets,
+                              xt::pytensor<DATA_T, 1> affinities_weights,
+                              const int numberOfThreads
+                      ) {
+                          array::StaticArray<uint64_t, DIM> shape;
+
+//                          std::array<int,DIM> shape;
+                          // Check inputs:
+                          for(auto d=0; d<DIM; ++d){
+                              shape[d] = labels.shape()[d];
+                              NIFTY_CHECK_OP(shape[d],==,affinities.shape()[d], "affinities have wrong shape");
+                          }
+                          NIFTY_CHECK_OP(offsets.shape()[0],==,affinities.shape()[DIM], "Affinities and offsets do not match");
+                          NIFTY_CHECK_OP(offsets.shape()[0],==,affinities_weights.shape()[0], "Affinities weights and offsets do not match");
+
+
+                          // Create thread pool:
+                          nifty::parallel::ParallelOptions pOpts(numberOfThreads);
+                          nifty::parallel::ThreadPool threadpool(pOpts);
+                          const std::size_t actualNumberOfThreads = pOpts.getActualNumThreads();
+
+                          // Create temporary arrays
+                          const auto nb_nodes = uint64_t(graph.nodeIdUpperBound()+1);
+                          xt::pytensor<DATA_T, 2> accAff = xt::zeros<DATA_T>({actualNumberOfThreads, nb_nodes});
+                          xt::pytensor<DATA_T, 2> maxAff = xt::zeros<DATA_T>({actualNumberOfThreads, nb_nodes});
+                          xt::pytensor<DATA_T, 2> counter = xt::zeros<DATA_T>({actualNumberOfThreads, nb_nodes});
+                          {
+                              py::gil_scoped_release allowThreads;
+                              nifty::tools::parallelForEachCoordinate(threadpool,
+                                                                      shape,
+                                                                      [&](const auto threadId, const auto & coordP){
+                                                                          const auto u = labels[coordP];
+                                                                          for(auto i=0; i<offsets.shape()[0]; ++i){
+                                                                              auto coordQ = coordP;
+                                                                              for (auto d=0; d<DIM; ++d) {
+                                                                                  coordQ[d] += offsets(i,d);
+                                                                              }
+                                                                              if(coordQ.allInsideShape(shape)){
+                                                                                  const auto v = labels[coordQ];
+                                                                                  if(u == v && u > 0 && v > 0){
+                                                                                          array::StaticArray<int64_t, DIM + 1> affIndex;
+                                                                                          std::copy(std::begin(coordP), std::end(coordP), std::begin(affIndex));
+                                                                                          affIndex[DIM] = i;
+                                                                                          const auto aff_value = affinities[affIndex];
+                                                                                          if (aff_value > maxAff(threadId, u))
+                                                                                              maxAff(threadId, u) = aff_value;
+                                                                                          counter(threadId,u) += affinities_weights(i);
+                                                                                          accAff(threadId,u) += aff_value*affinities_weights(i);
+                                                                                  }
+                                                                              }
+                                                                          }
+                                                                      });
+                          }
+
+                          // Create outputs:
+                          typedef xt::pytensor<DATA_T, 1> OutArrayType;
+                          typedef std::tuple<OutArrayType, OutArrayType, OutArrayType>  OutType;
+                          OutArrayType accAff_out = xt::zeros<DATA_T>({nb_nodes});
+                          OutArrayType counter_out = xt::zeros<DATA_T>({nb_nodes});
+                          OutArrayType maxAff_out = xt::zeros<DATA_T>({nb_nodes});
+
+                          // Normalize:
+                          for(auto i=0; i<nb_nodes; ++i){
+                              maxAff_out(i) = maxAff(0,i);
+                              for(auto thr=1; thr<numberOfThreads; ++thr){
+                                  counter(0,i) += counter(thr,i);
+                                  accAff(0,i) += accAff(thr,i);
+                                  if (maxAff(thr,i) > maxAff_out(i))
+                                      maxAff_out(i) = maxAff(thr,i);
+                              }
+                              if(counter(0,i)>0.5){
+                                  accAff_out(i) = accAff(0,i) / counter(0,i);
+                                  counter_out(i) = counter(0,i);
+                              } else {
+                                  accAff_out(i) = 0.;
+                                  counter_out(i) = 0.;
+                              }
+                          }
+                          return OutType(accAff_out, counter_out, maxAff_out);;
+                      },
+                      py::arg("graph"),
+                      py::arg("labels"),
+                      py::arg("affinities"),
+                      py::arg("offsets"),
+                      py::arg("affinitiesWeights"),
+                      py::arg("numberOfThreads") = -1
+
+        );
     }
 
 
